12c35156b4007094eef546f21e880f0f17d99389
MReiss97 <maxreiss21@gmail.com>
2024-05-28 13:46:56 -0400

    finished contact form styling

--- contact.php
+++ contact.php
@@ -14,7 +14,7 @@ $contact->ajax = true;
 $contact->to = $receiving_email_address;
 $contact->from_name = $_POST['name'];
 $contact->from_email = $_POST['email'];
-$contact->subject = 'DHA Website Contact ' . $_POST['organization'];
+$contact->subject = 'Max Reiss Website Contact ' . $_POST['organization'];
 
 // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
 /*
